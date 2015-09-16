08bb9f5ef6038c479d2a93f899b7464c79ab919b
nilskingston <nilskingston@hanovernorwichschools.org>
2015-09-16 13:42:34 -0400

    Updated from Brython Server: 9/16/2015 1:42:35 PM

--- birthday.py
+++ birthday.py
@@ -1,7 +1,7 @@
 """
 birthday.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Nils Kingston
+Credit: Roger
 Assignment:
 
 Your program will ask the user the following questions, in this order:
@@ -31,3 +31,6 @@ Example Session
   And the day? 11
   Eric, you are a fall baby of the stone age.
 """
+name = input("Hello, what is your name? ")
+month = input("Hi " + name + ", what was the name of the month you were born in? ")
+day = input("And what year were you born in, " + name + "? ")
